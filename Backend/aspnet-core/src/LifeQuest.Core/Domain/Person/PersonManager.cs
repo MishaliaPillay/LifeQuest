6447b50b21af84eeabf236ed25c124b54d71d72f
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-04-30 10:14:04 +0200

    hosted

--- Backend/aspnet-core/src/LifeQuest.Core/Domain/Person/PersonManager.cs
+++ Backend/aspnet-core/src/LifeQuest.Core/Domain/Person/PersonManager.cs
@@ -3,10 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Abp.Domain.Services;
 
 namespace LifeQuest.Domain.Person
 {
-    internal class PersonManager
+    public class PersonManager : DomainService
     {
     }
 }
--- Backend/aspnet-core/src/LifeQuest.EntityFrameworkCore/EntityFrameworkCore/LifeQuestDbContext.cs
+++ Backend/aspnet-core/src/LifeQuest.EntityFrameworkCore/EntityFrameworkCore/LifeQuestDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Abp.Zero.EntityFrameworkCore;
+﻿using System.Linq;
+using System;
+using Abp.Zero.EntityFrameworkCore;
 using LifeQuest.Authorization.Roles;
 using LifeQuest.Authorization.Users;
 using LifeQuest.MultiTenancy;
@@ -14,4 +16,36 @@ public class LifeQuestDbContext : AbpZeroDbContext<Tenant, Role, User, LifeQuest
         : base(options)
     {
     }
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+
+    {
+
+        base.OnModelCreating(modelBuilder);
+
+        // Force all DateTime and DateTime? to be treated as UTC
+
+        foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+
+        {
+
+            foreach (var property in entityType.GetProperties()
+
+                .Where(p => p.ClrType == typeof(DateTime) || p.ClrType == typeof(DateTime?)))
+
+            {
+
+                property.SetValueConverter(new Microsoft.EntityFrameworkCore.Storage.ValueConversion.ValueConverter<DateTime, DateTime>(
+
+                    v => v.Kind == DateTimeKind.Utc ? v : v.ToUniversalTime(),
+
+                    v => DateTime.SpecifyKind(v, DateTimeKind.Utc)
+
+                ));
+
+            }
+
+        }
+
+    }
+
 }
