4eb289e82b44372ded7b77baad11fa2a7eea2ce3
Mishalia Pillay <Mishalia.Pillay@boxfusion.io>
2025-04-30 14:30:19 +0200

    can create person without plan

---
+++ Backend/aspnet-core/src/LifeQuest.Application/Services/PersonService/Mappings/CustomDtoMappings.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using LifeQuest.Authorization.Users;
+using LifeQuest.Domain.Person;
+using LifeQuest.Services.PersonService.Dtos;
+
+namespace LifeQuest.Services.PersonService.Mappings
+{
+    class CustomDtoMappings : Profile
+    {
+        public CustomDtoMappings()
+        {
+            CreateMap<Person, PersonDto>()
+            .ForMember(dest => dest.UserName, opt => opt.MapFrom(src => src.User.UserName));
+            CreateMap<Person, PersonResponseDto>();
+            CreateMap<User, UserResponseDto>(); // 👈 Add this if it doesn't exist
+
+            CreateMap<CreateUpdatePersonDto, Person>();
+        }
+    }
+}
